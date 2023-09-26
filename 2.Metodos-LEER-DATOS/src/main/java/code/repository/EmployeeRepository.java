d991ecea58868612d3194fc95a2743e2b6c8c809
Luis Hernando Soto Mesa <59453252+LUINFO89@users.noreply.github.com>
2023-09-26 17:51:04 -0500

    Actualizacion

--- 2.Metodos-LEER-DATOS/src/main/java/code/repository/EmployeeRepository.java
+++ 2.Metodos-LEER-DATOS/src/main/java/code/repository/EmployeeRepository.java
@@ -3,10 +3,7 @@ package code.repository;
 import code.model.Employee;
 import code.util.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +15,7 @@ public class EmployeeRepository implements Repository<Employee>{
 
     @Override
     public List<Employee> findAll() throws SQLException {
+
         List<Employee> employees = new ArrayList<>(); // Debes crear una lista para almacenar los empleados.
 
         try (Connection connection = getConnection(); // Obtener la conexión correctamente
@@ -25,12 +23,7 @@ public class EmployeeRepository implements Repository<Employee>{
              ResultSet myRes = myStamt.executeQuery("SELECT * FROM empleados")) {
 
             while (myRes.next()) {
-                Employee e = new Employee();
-                e.setId(myRes.getInt("id"));
-                e.setNombre(myRes.getString("nombre")); // Cambiado a getString para obtener una cadena.
-                e.setApellidos(myRes.getString("apellidos")); // Cambiado a getString.
-                e.setCorreo(myRes.getString("correo")); // Cambiado a getString.
-                e.setSalario(myRes.getInt("salario")); // Cambiado a getDouble para obtener un número decimal.
+                Employee e = createEmployee(myRes);
                 employees.add(e);
             }
         }
@@ -41,13 +34,28 @@ public class EmployeeRepository implements Repository<Employee>{
 
 
 
-
-
-
     @Override
     public Employee getById(Integer id) {
-        return null;
+        Employee employee = null;
+
+        try (Connection connection = getConnection();
+             PreparedStatement myStamt = connection.prepareStatement("SELECT * FROM employes WHERE id = ?")) {
+
+            myStamt.setInt(1, id);
+
+            try (ResultSet myRes = myStamt.executeQuery()) {
+                if (myRes.next()) {
+                    employee = createEmployee(myRes);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Manejar la excepción de alguna manera, por ejemplo, lanzar una excepción personalizada o devolver null.
+        }
+
+        return employee;
     }
+
 
     @Override
     public void save(Employee employee) {
@@ -57,5 +65,17 @@ public class EmployeeRepository implements Repository<Employee>{
     @Override
     public void delete(Integer id) {
 
+    }
+
+    //Metodo para crear empleados
+    private Employee createEmployee(ResultSet myRes) throws SQLException {
+        Employee e = new Employee();
+        e.setId(myRes.getInt("id"));
+        e.setNombre(myRes.getString("nombre")); // Cambiado a getString para obtener una cadena.
+        e.setApellidos(myRes.getString("apellidos")); // Cambiado a getString.
+        e.setCorreo(myRes.getString("correo")); // Cambiado a getString.
+        e.setSalario(myRes.getInt("salario")); // Cambiado a getDouble para obtener un número decimal.
+        //employees.add(e);
+        return e;
     }
 }

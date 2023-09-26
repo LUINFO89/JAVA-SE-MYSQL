7e529af218b28b5b8c14e924e20f7e95dd85b92d
Luis Hernando Soto Mesa <59453252+LUINFO89@users.noreply.github.com>
2023-09-26 13:56:37 -0500

    1.EstructuradelProyecto

---
+++ 1.EstructuraYConexion/src/main/java/code/main/main.java
@@ -0,0 +1,25 @@
+package code.main;
+
+import code.util.DatabaseConnection;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+
+public class main {
+    public static void main(String[] args) throws SQLException {
+
+
+
+        try (   Connection myConn = DatabaseConnection.getInstance();
+                Statement myStamt = myConn.createStatement();
+                ResultSet myRes = myStamt.executeQuery("SELECT * FROM usuarios");){
+
+                while (myRes.next()){
+                    System.out.println(myRes.getString("nombre"));
+                }
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Algo salio mal ");
+        }
+    }
+}

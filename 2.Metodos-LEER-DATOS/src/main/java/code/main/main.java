ecfd8ce61c553514842855a102323d6a3cdafafb
Luis Hernando Soto Mesa <luisinformatica.89@gmail.com>
2023-09-26 21:22:22 -0500

    Leer datos de la base de datos MYSQL

--- 2.Metodos-LEER-DATOS/src/main/java/code/main/main.java
+++ 2.Metodos-LEER-DATOS/src/main/java/code/main/main.java
@@ -1,5 +1,8 @@
 package code.main;
 
+import code.model.Employee;
+import code.repository.EmployeeRepository;
+import code.repository.Repository;
 import code.util.DatabaseConnection;
 
 import javax.swing.plaf.nimbus.State;
@@ -8,11 +11,17 @@ import java.sql.*;
 public class main {
     public static void main(String[] args) throws SQLException {
 
+        try (   Connection myConn = DatabaseConnection.getInstance()){
+
+            Repository<Employee> reopository = new EmployeeRepository();
+
+            reopository.findAll().forEach(System.out::println);
+        }
 
 
-        try (   Connection myConn = DatabaseConnection.getInstance();
+       /* try (   Connection myConn = DatabaseConnection.getInstance();
                 Statement myStamt = myConn.createStatement();
-                ResultSet myRes = myStamt.executeQuery("SELECT * FROM usuarios");){
+                ResultSet myRes = myStamt.executeQuery("SELECT * FROM empleados");){
                 System.out.println("Conexion de forma correcta ");
 
                 while (myRes.next()){
@@ -21,6 +30,6 @@ public class main {
         }catch (Exception e){
             e.printStackTrace();
             System.out.println("Algo salio mal ");
-        }
+        }*/
     }
 }

f417e6ebf2e93dbac0bc7cd5da1156f6a367c02f
Luis Hernando Soto Mesa <luisinformatica.89@gmail.com>
2023-09-27 10:37:12 -0500

    SWINGAPP

--- 6.ProyectoCRUD-GUI-VIEW/src/main/java/org/example/platzi/main/Main.java
+++ 6.ProyectoCRUD-GUI-VIEW/src/main/java/org/example/platzi/main/Main.java
@@ -3,17 +3,20 @@ package org.example.platzi.main;
 import org.example.platzi.model.Employee;
 import org.example.platzi.repository.EmployeeRepository;
 import org.example.platzi.repository.Repository;
+import org.example.platzi.view.SwingApp;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.println("---Listando todos----");
+        /*System.out.println("---Listando todos----");
         Repository<Employee> repository = new EmployeeRepository();
         repository.findAll().forEach(System.out::println);
 
         System.out.println("----Buscando por ID----");
-        System.out.println(repository.getById(2));
+        System.out.println(repository.getById(2));*/
+        SwingApp app = new SwingApp();
+        app.setVisible(true);
     }
 }
 
